21452ae1cd84b5fca53a63c3ac0a94167368c4cb
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-09 14:37:48 +0100

    Render tasks

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    { name: 'Buy shopping', completed: false},
+    { name: 'Clean bathroom', completed: true},
+    { name: ' Car\'s MOT', completed: false}
+  ])
+
+  const taskNodes = tasks.map((task, index) => {
+    return (
+      <li key={index}>
+        {task.name}
+      </li>
+    )
+  });
+
   return (
     <>
     <h1>
       ToDo's
     </h1>
     <ul>
-
+      { taskNodes }
     </ul>
 
     </>
