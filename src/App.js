41c9924d05b514232d83e59820192d2b98ac5481
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-09 15:30:56 +0100

    Add option to save task

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import './App.css';
 function App() {
 
   const [tasks, setTasks] = useState([
-    { name: 'Buy shopping', completed: false},
-    { name: 'Clean bathroom', completed: true},
-    { name: ' Car\'s MOT', completed: false}
+    { name: 'Buy shopping', priority: 'high'},
+    { name: 'Clean bathroom', priority: 'low'},
+    { name: ' Car\'s MOT', priority: 'high'}
   ])
 
   const taskNodes = tasks.map((task, index) => {
     return (
       <li key={index}>
-        {task.name};
+        {task.name}
       </li>
     )
   });
@@ -22,14 +22,40 @@ function App() {
 
   const handleTaskInput = (event) => setNewTask(event.target.value);
 
+  const saveNewTask = (event) => {
+    event.preventDefault();
+
+    if (newTask === '') {
+      return;
+    }
+
+    const copyTasks = [... tasks, {name: newTask, completed: false}];
+    setTasks(copyTasks);
+    setNewTask('');
+  }
+
+
   return (
     <>
     <h1>
       ToDo's
     </h1>
-    <form>
+    <form onSubmit={saveNewTask}>
+    {/* Text input */}
       <input type='text' placeholder='Type in new task' name='next-task' onChange={handleTaskInput} value={newTask}></input>
       <label htmlFor='new-task'></label>
+
+    <span>
+      {/* First radio */}
+      <input type='radio' name='priority' value='high'></input>
+      <label htmlFor='high'>High</label>
+
+      {/* Second radio */}
+      <input type='radio' name='priority' value='low'></input>
+      <label htmlFor='low'>Low</label>
+    </span>
+
+    {/* Submit button */}
       <input type='submit' value='Add task'></input>
     </form>
     <ul>
